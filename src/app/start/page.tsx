6a26132cf4527a242c23f84fb30b7a75d66f8e8d
dayal <dayal.gaitonde@gmail.com>
2024-12-19 14:59:09 -0800

    md change for vercel test

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -125,7 +125,7 @@ export default function StartPage() {
               className="text-blue-500 hover:underline"
               prefetch={true}
             >
-              Sign in
+              Sign in_
             </Link>
           </span>
         </div>
